fea82eb2ec2c915d6f9434556e8a2a6b52e64e65
Max Kucher <maksim@kuchermg.ru>
2025-08-07 17:55:15 +0300

    feat: настроить PostHog телеметрию для сборки VSIX
    - Добавить новое событие VSIX_BUILD_COMPLETED в TelemetryEventName
    - Создать скрипт send-vsix-build-event.js для отправки событий в PostHog
    - Добавить dotenv для загрузки переменных окружения
    - Модифицировать скрипт vsix для автоматической отправки события после сборки
    - Добавить posthog-node в devDependencies
    - Создать .env файлы с правильными PostHog ключами
    - Скрипт отправляет информацию о версии, размере файла, платформе и времени сборки

--- packages/types/src/telemetry.ts
+++ packages/types/src/telemetry.ts
@@ -66,6 +66,8 @@ export enum TelemetryEventName {
 	SHELL_INTEGRATION_ERROR = "Shell Integration Error",
 	CONSECUTIVE_MISTAKE_ERROR = "Consecutive Mistake Error",
 	CODE_INDEX_ERROR = "Code Index Error",
+
+	VSIX_BUILD_COMPLETED = "VSIX Build Completed",
 }
 
 /**
@@ -167,6 +169,7 @@ export const rooCodeTelemetryEventSchema = z.discriminatedUnion("type", [
 			TelemetryEventName.TAB_SHOWN,
 			TelemetryEventName.MODE_SETTINGS_CHANGED,
 			TelemetryEventName.CUSTOM_MODE_CREATED,
+			TelemetryEventName.VSIX_BUILD_COMPLETED,
 		]),
 		properties: telemetryPropertiesSchema,
 	}),
